9a10f2ddcb4c491561cc5f5eccb70779fb0064b3
Vishal262002 <128057703+Vishal262002@users.noreply.github.com>
2024-04-08 03:27:01 -0700

    Delete notify.py

--- notify.py
+++
@@ -1,6 +0,0 @@
-from plyer import notification
-notification.notify(title='Title',
-                    message='Hello',
-                    app_icon=None,
-                    app_name='Zomato',
-                    timeout=5)
